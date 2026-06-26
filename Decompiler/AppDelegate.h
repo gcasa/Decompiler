947ded8551069fd5e4bb6277fddde9c2648634f1
Gregory Casamento <greg.casamento@gmail.com>
2026-06-26 17:08:02 -0400

    Initial Commit

---
+++ Decompiler/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Decompiler
+//
+//  Created by Gregory Casamento on 6/26/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Decompiler/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Decompiler
+//
+//  Created by Gregory Casamento on 6/26/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
